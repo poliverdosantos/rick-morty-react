01b7539190fee2cb68fc44d1ba1588c3fdf4b0bf
Oliver Pereira <oliver.pereira@jala.university>
2024-05-15 16:25:05 -0400

    feat: Add Characters component to display character information

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import imagenRickMorty from './img/rick-morty.png';
-
+import Characters from './components/Characters';
 
 
 function App() {
@@ -15,12 +15,15 @@ function App() {
     const characterApi = await api.json();
 
     setCharacters(characterApi.results);
+
+
   };
 
     return (
     <div className="App">
       <header className="App-header">
        <h1 className="title"> Rick & Morty </h1>
+       <Characters/>
        <img src = {imagenRickMorty} alt="Rick & Morty" className='imgRickMorty'/>
        <button onClick={reqApi} className="btn-search">Buscar Personaje</button>
       </header>
---
+++ src/components/Characters.js
@@ -0,0 +1,6 @@
+export default function characters()
+{
+    <div>
+        <h1>Desde characters JS</h1>        
+    </div>
+}
