7a86c9c0c4f053f157130f715b8f2b52f01e1808
Oliver Pereira <oliver.pereira@jala.university>
2024-05-15 16:21:19 -0400

    feat: Add State with dates in App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import imagenRickMorty from './img/rick-morty.png';
 
@@ -5,15 +6,18 @@ import imagenRickMorty from './img/rick-morty.png';
 
 function App() {
 
+  const [characters, setCharacters] = useState(null);
+
   const reqApi = async()=>{
+
+
     const api = await fetch('https://rickandmortyapi.com/api/character');
     const characterApi = await api.json();
-    console.log(characterApi);
 
-
+    setCharacters(characterApi.results);
   };
 
-  return (
+    return (
     <div className="App">
       <header className="App-header">
        <h1 className="title"> Rick & Morty </h1>
