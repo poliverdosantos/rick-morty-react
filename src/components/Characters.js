ae870ac6a471c5abd8ddacf8cb6fc2676f1429fd
Oliver Pereira <oliver.pereira@jala.university>
2024-05-15 16:56:34 -0400

    feat: Improve Characters component to display character information

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -8,7 +8,31 @@ export default function characters(props)
             <div className="container-characters">
                 {characters.map((character, index) => (
                     <div className="character-container" key={index}> 
-                        <p>{character.name}</p>
+                        <div>
+                            <img src={character.image} alt={character.name}/>
+                        </div>
+                        <div>
+                            <h3>{character.name}</h3>
+                            <h6>
+                                {character.status === "Alive"?(
+                                    <>
+                                    <span className="alive"/>
+                                    Alive
+                                    </>
+                                    ):(
+                                    <>
+                                    <span className="dead"/>
+                                    Dead
+                                    </>
+                                )}
+                            </h6>
+                            <p>
+                                <span  className="text-grey"> Episodios: </span>
+                                <span>{character.episode.length}</span>
+                            </p>
+
+                        </div>
+
                     </div>
                 ))}                  
             </div>
