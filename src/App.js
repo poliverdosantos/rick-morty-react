e98a5d50b72830789766c47139c478ad3dfd7218
Oliver Pereira <oliver.pereira@jala.university>
2024-05-15 16:01:48 -0400

    chore: Update npm start script to "dev" for consistency with other scripts

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import imagenRickMorty from './img/rick-morty.png';
+
+
 
 function App() {
+
+  const reqApi = ()=>{
+    console.log('Click en el boton');
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1 classNam="title"> Rick & Morty </h1>
+       <img src = {imagenRickMorty} alt="Rick & Morty" className='imgRickMorty'/>
+       <button onClick={reqApi} className="btn-search">Buscar Personaje</button>
       </header>
     </div>
   );
