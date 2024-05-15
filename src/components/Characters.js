d6a9c1abe28b5d9dbe8f3553ef9f824fe38e5772
Oliver Pereira <oliver.pereira@jala.university>
2024-05-15 16:44:51 -0400

    feat: Update Characters component to display character names

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,8 +1,18 @@
 export default function characters(props)
 { 
+    const {characters} = props;
+    
     return(
-        <div>
-            <h4>Desde characters</h4>            
+        <div className="characters">
+            <h1 className="back-home">Volver a la Home</h1>            
+            <div className="container-characters">
+                {characters.map((character, index) => (
+                    <div className="character-container" key={index}> 
+                        <p>{character.name}</p>
+                    </div>
+                ))}                  
+            </div>
+            
         </div>
     );
 }
