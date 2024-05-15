3fe33af6cd0a87b1c0e2dd9d8801cc2edb05224b
Oliver Pereira <oliver.pereira@jala.university>
2024-05-15 16:09:16 -0400

    feat: Add async/await to API request in App.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import imagenRickMorty from './img/rick-morty.png';
 
 function App() {
 
-  const reqApi = ()=>{
-    console.log('Click en el boton');
+  const reqApi = async()=>{
+    const api = await fetch('https://rickandmortyapi.com/api/character');
+    const characterApi = await api.json();
+    console.log(characterApi);
+
+
   };
 
   return (
     <div className="App">
       <header className="App-header">
-       <h1 classNam="title"> Rick & Morty </h1>
+       <h1 className="title"> Rick & Morty </h1>
        <img src = {imagenRickMorty} alt="Rick & Morty" className='imgRickMorty'/>
        <button onClick={reqApi} className="btn-search">Buscar Personaje</button>
       </header>
